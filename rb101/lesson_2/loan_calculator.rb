e6ae683dd0854c933f2e56dc56e7f9234c73fbd1
Khalid Hussain <Khalid@KhalidHussain.com>
2020-09-20 03:33:27 +0000

    modified loan_calculator.rb

--- rb101/lesson_2/loan_calculator.rb
+++ rb101/lesson_2/loan_calculator.rb
@@ -7,58 +7,82 @@ loan_amount = nil
 apr = nil
 loan_duration_years = nil
 
-def valid_integer?(loan_amount)
-  loan_amount == loan_amount.to_i.to_s
-end
-
-def valid_float?(apr)
-  apr == apr.to_f.to_s
+def valid_number?(number)
+  (number == number.to_i.to_s || number == number.to_f.to_s) &&
+    (number.to_i >= 0 || number.to_f >= 0)
 end
 
 def calculate_payment(loan_amount, monthly_interest_rate, loan_duration_months)
-  loan_amount * (monthly_interest_rate /
-                (1 - (1 + monthly_interest_rate)**(-loan_duration_months)))
-end
-
-loop do
-  print "Please enter the loan amount as an integer: "
-  loan_amount = gets.chomp
-  if valid_integer?(loan_amount)
-    loan_amount = loan_amount.to_i
-    break
+  if monthly_interest_rate == 0
+    loan_amount / loan_duration_months
   else
-    puts "Invalid input."
+    loan_amount * (monthly_interest_rate /
+                  (1 - (1 + monthly_interest_rate)**(-loan_duration_months)))
   end
 end
 
-loop do
-  print "Please enter the APR amount as a float: "
-  apr = gets.chomp
-  if valid_float?(apr)
-    apr = apr.to_f / 100
-    break
-  else
-    puts "Invalid input."
-  end
-end
+puts "Welcome to the Loan Calculator! \n\n"
 
 loop do
-  print "Please enter the loan duration in full years: "
-  loan_duration_years = gets.chomp
-  if valid_integer?(loan_duration_years)
-    loan_duration_years = loan_duration_years.to_i
+  loop do
+    print "Please enter the loan amount: "
+    loan_amount = gets.chomp
+    if valid_number?(loan_amount)
+      loan_amount = loan_amount.to_f
+      break
+    else
+      puts "Invalid input."
+    end
+  end
+
+  loop do
+    print "Please enter the APR amount: "
+    apr = gets.chomp
+    if valid_number?(apr)
+      apr = apr.to_f / 100
+      break
+    else
+      puts "Invalid input."
+    end
+  end
+
+  loop do
+    print "Please enter the loan duration in years: "
+    loan_duration_years = gets.chomp
+    if valid_number?(loan_duration_years)
+      loan_duration_years = loan_duration_years.to_f
+      break
+    else
+      puts "Invalid input."
+    end
+  end
+
+  monthly_interest_rate = (apr / 12)
+  loan_duration_months = loan_duration_years * 12
+
+  puts "Your monthly interest rate is : #{monthly_interest_rate.round(3)}%"
+  puts "The loan duration in months is : #{loan_duration_months}"
+  puts "Your monthly payment is
+        #{calculate_payment(loan_amount, monthly_interest_rate,
+                            loan_duration_months).round(2)}"
+
+  answer = ''
+
+  puts "\n"
+
+  loop do
+    print "Do you want to perform another calculation? ('y' or 'n') "
+    answer = gets.chomp.downcase
+    if answer == 'y' || answer == 'n' || answer == 'yes' || answer == 'no'
+      break
+    else
+      puts "Invalid answer."
+    end
+  end
+
+  if answer == 'n' || answer == 'no'
     break
   else
-    puts "Invalid input."
+    puts "\n"
   end
 end
-
-monthly_interest_rate = (apr / 12)
-loan_duration_months = loan_duration_years * 12
-
-puts "Your monthly interest rate is : #{monthly_interest_rate.round(3)}%"
-puts "The loan duration in months is : #{loan_duration_months}"
-puts "Your monthly payment is #{calculate_payment(loan_amount,
-                                                  monthly_interest_rate,
-                                                  loan_duration_months).round(2)
-                              }"
